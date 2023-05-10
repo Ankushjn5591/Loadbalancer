48d49853a3d777a7427651a460f955d04f3bcb95
Ankush Jain <ankushjn38@gmaial.com>
2023-05-10 18:00:56 +0530

    commit

--- Main.tf
+++ Main.tf
@@ -47,42 +47,9 @@ resource "azurerm_lb" "lb" {
     public_ip_address_id          = azurerm_public_ip.lbip.id
     private_ip_address_allocation = "Dynamic"
 }
-  backend_address_pool {
-    name = "lbbackend"
-}  
-
-probe {
-    name                      = "lbhealthprobe"
-    protocol                  = "Tcp"
-    port                      = 80
-    interval                  = 15
-    number_of_probes          = 2
-    request_path              = "/"
-    unhealthy_threshold       = 2
-    timeout_seconds           = 10
-  }
-
-  load_balancing_rule {
-    name                   = "lbrule"
-    frontend_ip_configuration_name = "lbfrontendip"
-    backend_address_pool_id         = "lbbackend"
-    protocol                         = "Tcp"
-    frontend_port                    = 80
-    backend_port                     = 80
-    enable_floating_ip               = false
-    idle_timeout_in_minutes          = 5
-    probe_id                          = "lbhealthprobe"
-  }
-}
-
-resource "azurerm_network_interface_backend_address_pool_association"  "nic" {
-  network_interface_id = data.azurerm_network_interface_nic.name
-  ip_configuration_name = data.azurerm_public_ip.pip.name
-  backend_address_pool_id = "lbbackend"
 }
 
 
- 
 terraform {
   backend "azurerm" {
     resource_group_name  = "Storagerg"
